0c5ba20c04b79ead3b45dc1011460e93864d4d74
Александр <aleksandrlitvinovgh@gmail.com>
2025-07-04 11:18:01 +0300

    base architechture init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+import { CronModule } from './cron/cron.module';
+import { ParserService } from './parser/parser.service';
+import { ParserModule } from './parser/parser.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, RabbitmqModule, CronModule, ParserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ParserService],
 })
 export class AppModule {}
